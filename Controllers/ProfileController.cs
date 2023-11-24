4c533bd6e9d3efa499cbd11972b98b37ce3e19da
mehmetncr <mehmet.ncar@gmail.com>
2023-11-24 17:04:33 +0300

    level1

---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore_Social_Network_UI.Controllers
+{
+	public class ProfileController : Controller
+	{
+		public IActionResult MyProfile()
+		{
+			return View();
+		}
+		public IActionResult MyPhotos()
+		{
+			return View();
+		}
+	}
+}
