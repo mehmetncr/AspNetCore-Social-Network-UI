0295e588bf62c00603866aee8f57f0edbf9ba667
Emre Boyabat <128058104+emrebybt@users.noreply.github.com>
2023-12-08 21:30:42 +0300

    added createpost

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -1,16 +1,55 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspNetCore_Social_Network_UI.Models;
+using AspNetCore_Social_Network_UI.SessionExtensions;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
 
 namespace AspNetCore_Social_Network_UI.Controllers
 {
     public class PostController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public PostController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
-        public IActionResult AddPost()
+        public async Task<IActionResult> AddPost(PostViewModel model)
         {
-            return View();
+            if(model.PostYoutubeUrl != null)
+            {
+                model.PostType = "Youtube";
+            }
+            if (model.PostImageUrl != null) //IFromFile kontrolüne çevirilecek
+            {
+                model.PostType = "Image";
+            }
+            if(model.PostVideoUrl != null)  //Video kontrolüne çevilecek
+            {
+                model.PostType = "Video";
+            }
+            else
+            {
+                model.PostType = "Text";
+            }
+            var user = HttpContext.Session.GetJsonUser();
+            model.PostUserId = user.UserId;
+            var http = _httpClientFactory.CreateClient();
+            var jsondata = JsonConvert.SerializeObject(model);
+			var content = new StringContent(jsondata, encoding: Encoding.UTF8, "application/json");
+            var result = await http.PostAsync("https://localhost:7091/api/Post/AddPost", content);
+            if (result.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+			var errorMessage = await result.Content.ReadAsStringAsync();
+			return RedirectToAction("Index", "Home");
         }
     }
 }
---
+++ ViewComponents/NewPostViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using AspNetCore_Social_Network_UI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore_Social_Network_UI.ViewComponents
+{
+    public class NewPostViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            PostViewModel model = new PostViewModel();
+            return View(model);
+        }
+    }
+}
