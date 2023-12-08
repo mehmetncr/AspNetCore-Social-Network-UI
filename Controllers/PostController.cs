7650811910241df366234d29be5ec10fc56b5cf2
Emre Boyabat <128058104+emrebybt@users.noreply.github.com>
2023-12-08 18:29:46 +0300

    add online friends

---
+++ Controllers/PostController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore_Social_Network_UI.Controllers
+{
+    public class PostController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddPost()
+        {
+            return View();
+        }
+    }
+}
